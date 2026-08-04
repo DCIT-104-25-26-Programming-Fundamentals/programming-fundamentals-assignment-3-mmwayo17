caf1d71cd13e01b8970ec7ace9287a5d4b51d877
mmwayo17 <mmwayo_seini001@st.ug.edu.gh>
2026-08-04 13:45:41 +0000

    finished assignment 3

--- assignment_03_array_statistics.js
+++ assignment_03_array_statistics.js
@@ -42,5 +42,67 @@
 // =============================================================================
 // YOUR CODE BELOW — remove the // symbols from the scaffold and fill it in
 // =============================================================================
+import readlineSync from 'readline-sync'
+
+function sum(nums){
+    let sum = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        sum += nums[i];
+    }
+    return sum;
+}
+
+function average(nums){
+    let avg = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        avg += nums[i];
+    }
+    return avg/nums.length;
+}
+
+function maximum(nums){
+    let max = 0;
+
+    for(let i =0; i < nums.length; i++){
+        if(nums[i] > max){
+            max = nums[i];
+        }
+    }
+    return max;
+}
+
+function minimun(nums){
+    let mini = nums[0];
+
+    for(let i =0; i < nums.length; i++){
+        if(nums[i] < mini){
+            mini = nums[i];
+        }
+    }
+    return mini;
+}
 
 
+function main(){
+    let numbers = [];
+    const amount = readlineSync.questionInt("How many numbers? ");
+
+    if(amount < 1){
+        console.log("Invalid number.");
+        return;
+    }
+    
+    for(let i =0; i < amount; i++){
+        numbers.push(readlineSync.questionInt(`Enter number ${i + 1}: `))
+    }
+
+    console.log("Results: ");
+    console.log(`Sum:     ${sum(numbers)}`);
+    console.log(`Average: ${average(numbers)}`);
+    console.log(`Maximum: ${maximum(numbers)}`);
+    console.log(`Minimum: ${minimun(numbers)}`);
+}
+
+main()
