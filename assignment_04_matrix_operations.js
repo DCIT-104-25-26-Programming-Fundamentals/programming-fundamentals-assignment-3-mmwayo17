8be4e19a8ae4f018cebdbee7d4291933a2fcb0e0
mmwayo17 <mmwayo_seini001@st.ug.edu.gh>
2026-08-06 21:35:33 +0000

    finished assignment 4

--- assignment_04_matrix_operations.js
+++ assignment_04_matrix_operations.js
@@ -70,3 +70,113 @@
 
 const readlineSync = require('readline-sync');
 
+function createMatirx(matrix, row, column){
+    for(let i = 0; i < row; i++){
+        const values = readlineSync.question(`Enter row ${i +  1}: `).split(" ");
+        matrix.push([]);
+        for(let j =0; j < column; j++){
+            matrix[i].push(Number(values[j]));
+        }
+    }
+
+    return matrix;
+}
+
+function print_matrix(matrix){
+    stringMatrix = "";
+    for(let i = 0; i < matrix.length; i++){
+        for(let j = 0; j < matrix[0].length; j++){
+            stringMatrix += `${matrix[i][j]}   `;
+        }
+        console.log(stringMatrix);
+        stringMatrix = "";
+    }
+    console.log("------------");
+}
+
+function transposeMatrix(matrix){
+    let transposedMatrix = [];
+
+    for(let i = 0; i < matrix[0].length; i++){
+        transposedMatrix.push([]);
+    }
+
+    for(let i = 0; i < matrix.length; i++){  
+        for(let j = 0; j < matrix[i].length; j++){
+            transposedMatrix[j].push(matrix[i][j]);
+        }
+    }
+
+    return transposedMatrix;
+}
+
+function addMatrix(matrix1, matrix2){
+    let addedMatrix = []
+    if (matrix1.length !== matrix2.length || matrix1[0].length !== matrix2[0].length){
+        console.log("Can't add matrices of uneven size.")
+        return;
+    }
+
+    for(let i = 0; i < matrix1.length; i++){
+        addedMatrix.push([])
+    }
+
+    for(let i = 0; i < matrix1.length; i++){
+        for(let j = 0; j < matrix1[0].length; j++){
+            addedMatrix[i].push(matrix1[i][j] + matrix2[i][j]);
+        }
+    }
+
+    return addedMatrix;
+}
+
+function multiplyMatrix(matrix1, matrix2){
+    let multipliedMatrix = [];
+    if(matrix1[0].length !== matrix2.length){
+        console.log("Cannot multiply matrices");
+        return;
+    }
+
+    for(let i = 0; i < matrix1.length; i++){
+        multipliedMatrix.push([])
+    }
+
+    for(let i = 0; i < matrix1.length; i++){
+        for(let j = 0; j < matrix1.length; j++){
+            let sum = 0;
+            for(let k = 0; k < matrix1.length; k++){
+                sum += matrix1[i][k] * matrix2[k][j]
+            }
+            multipliedMatrix[i].push(sum)
+        }
+    }
+
+    return multipliedMatrix;
+}
+
+function main(){
+    let matrix = [];
+
+    const rows = readlineSync.questionInt("Enter number of rows: ");
+    const columns = readlineSync.questionInt("Enter number of columns: ");
+
+    matrix = createMatirx(matrix, rows, columns);
+    const transposed = transposeMatrix(matrix);
+    const added = addMatrix(matrix, matrix);
+    const multiplied = multiplyMatrix(matrix, matrix);
+
+    console.log("Original Matrix:");
+    print_matrix(matrix);
+
+    console.log("Transposed Matrix:");
+    print_matrix(transposed);
+
+    console.log("Added matrix:");
+    print_matrix(added);
+
+    console.log("Multiplied Matrix:");
+    print_matrix(multiplied);
+}
+
+//Note, I used the same matrix when performing the addition and multiplication to keep the program simple.
+main()
